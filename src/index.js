65db3b184690feea7a1a2d34830c5eb35c342151
Fredys Fernandez <juniorfrey2990@gmail.com>
2021-11-09 09:36:17 -0500

    Callback Hell

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import { buscarHeroe } from './js/callaback'
 import './style.css';
-const heoreId = 'iron';
+const heoreId1 = 'capi';
+const heoreId2 = 'spider';
 
 
-buscarHeroe(heoreId, (heroe) => {
-    console.log(heroe);
+buscarHeroe(heoreId1, (err, heroe1) => {
+    if(err){console.error(err);}
+
+    buscarHeroe(heoreId2, (err, heroe2) => {
+        if(err){console.error(err);}
+        console.log(`Enviamdo a ${heroe1.nombre} y ${heroe2.nombre} a la misión`);
+    })
 });
--- src/js/callaback.js
+++ src/js/callaback.js
@@ -17,7 +17,12 @@ export const buscarHeroe = (id, callback) => {
 
 
     const heroe = heroes[id];
+    if(heroe){
+        callback(null, heroe);
+    }else{
+        callback(`No existe un héroe con el id ${id}`);
+    }
 
-    callback(heroe)
+    //callback(heroe)
 
 }
